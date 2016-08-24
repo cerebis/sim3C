39db05d0ec027bfc9c50e66e62d03c79e15d8227
Matthew DeMaere <matt.demaere@gmail.com>
2016-08-24 12:57:10 +1000

    Addresses #32 #33
    There was a defect when joining channels, which then effected caching.

--- Helper.groovy
+++ Helper.groovy
@@ -82,14 +82,26 @@ class Helper {
         String name
         Object value
 
-        // required for serialization
+        /**
+         * A default constructor is required for Kryo deserialization.
+         */
         NamedValue() {/*...*/}
 
+        /**
+         * Basic constructor.
+         * @param name
+         * @param value
+         */
         NamedValue(name, value) {
             this.name = name
             this.value = value
         }
 
+        /**
+         * String representations are of the underlying values only, collections are
+         * concatenated by whitespace.
+         * @return String
+         */
         String toString() {
             if (value instanceof Collection) {
                 value.toSorted().join(' ')
@@ -103,6 +115,11 @@ class Helper {
             new NamedValue(name,value)
         }
 
+        /**
+         * NamedValue identity is determined by both name and value.
+         * @param other
+         * @return
+         */
         boolean equals(Object obj) {
             if (!(obj instanceof NamedValue)) {
                 return false
@@ -114,8 +131,12 @@ class Helper {
             return other.name == name && other.value == value
         }
 
+        /**
+         * NamedValue identity is determined by both name and value.
+         * @return
+         */
         int hashCode() {
-            return name.hashCode()
+            return Objects.hash(name, value)
         }
     }
 
@@ -123,8 +144,34 @@ class Helper {
         private static final int HASH_PRIME = 37
         LinkedHashMap varMap = [:]
 
-        // required for serialization
+        /**
+         * A default constructor is required for deserialization.
+         *
+         * NOTE. Though not required when no other constructors have been implemented, it has been added
+         * to avoid the possibility of introducing an unintended side-effect -- if one was implemented
+         * later. This would then break Kryo deserialization, which expects to be able toinstantiate
+         * objects without parameters.
+         */
         Key() {/*...*/}
+
+        /**
+         * Analogous to String concatenation, the + operator is not reflexive. (a+b != b+a)
+         *
+         * @param other
+         * @return Key
+         */
+        @Synchronized
+        Key plus(Key other) {
+            Key cpy = new Key()
+            cpy.varMap.putAll(this.varMap)
+            cpy.varMap.putAll(other.varMap)
+            return cpy
+        }
+
+        @Synchronized
+        List values() {
+            varMap.collect{ k, v -> v}
+        }
 
         @Synchronized
         Key copy() {
@@ -167,7 +214,7 @@ class Helper {
         }
 
         String id(int maxDepth) {
-            def values = varMap.values() as List
+            List values = varMap.values() as List
             assert maxDepth > 0 : 'Depth must be a positive integer'
             assert maxDepth <= values.size() : "Requested depth [$maxDepth] exceeds defined variable count [${values.size()}]"
             return id(values[0..maxDepth-1])
@@ -177,6 +224,11 @@ class Helper {
             return id(varMap.values())
         }
 
+        /**
+         * Key identity considers only the keys in the contained Map (varMap)
+         * @param other
+         * @return
+         */
         boolean equals(Object other) {
             if (!(other instanceof Key)) {
                 return false
@@ -185,34 +237,57 @@ class Helper {
                 return true
             }
             Key _o = other as Key
-            return _o.varMap.keySet().equals(varMap.keySet())
+
+            return _o.toString().equals(toString())
         }
 
+        /**
+         * Key identity considers only the keys in the contained Map (varMap)
+         * @return
+         */
         int hashCode() {
-            return this.toString().hashCode()
+            return Objects.hash(toString())
         }
 
         public def getKeys() {
             varMap.keySet() as List
         }
 
-        List orderedJoin(Key other) {
-            List a = getKeys() as List
-            List b = other.getKeys() as List
-            List ret = []
-            for (int i = 0; i < a.size() && i < b.size(); i++) {
-                if (a[i] != b[i]) {
-                    break
-                }
-                ret[i] = a[i]
-            }
-            return ret
-        }
-
         List join(Key other) {
             getKeys().intersect(other.getKeys()) as List
         }
 
+        /**
+         * Return a subkey, where the depth is less than the maximum depth of the sweep.
+         * @param depth - the depth to retain
+         * @return reduced key
+         */
+        public Key subKey(int depth) {
+            List values = varMap.values() as List
+            assert depth > 0 : 'Depth must be a positive integer'
+            assert depth <= values.size() : "Requested depth [$depth] exceeds defined variable count [${values.size()}]"
+            Key k = new Key()
+            k.putAll(values[0..(depth-1)])
+            return k
+        }
+
+        /**
+         * At a given sweep level, split a key into two parts.
+         *
+         * Neither part can be empty, that is you cannot split at the lowest or highest level.
+         *
+         * @param level - the sweep level at which to split (0 > level < max_level)
+         * @return Map containing two keys: 'hi' and 'lo'
+         */
+        public Map splitKey(int level) {
+            def values = varMap.values() as List
+            assert level > 0 : 'Depth must be > 0'
+            assert level < values.size() : "Requested cut point [$level] would produce an empty split for max [${values.size()}] levels"
+            def keys = ['lo':new Key(), 'hi':new Key()]
+            keys.lo.putAll(values[0..level-1])
+            keys.hi.putAll(values[level..-1])
+            return keys
+        }
     }
 
 
@@ -273,9 +348,9 @@ class Helper {
             // iterate from current collection and copy 'current' element N times, one for each element
             def currentCollection = ori[d];
             for (element in currentCollection) {
-                List copy = current.collect()
-                copy.add(element);
-                permuteImpl(ori, res, d + 1, copy)
+                List cpy = current.collect()
+                cpy.add(element);
+                permuteImpl(ori, res, d + 1, cpy)
             }
         }
 
@@ -293,44 +368,112 @@ class Helper {
 
             df = df.spread(p)
                     .map { row ->
-                        def key = row[0].copy();
-                        def vars = row[-varNames.size()..-1]
-                        vars.each { key.put(it) }
+                def key = row[0].copy();
+                def vars = row[-varNames.size()..-1]
+                vars.each { key.put(it) }
 
-                        proc_vars = []
-                        int num_proc_vars = row.size() - varNames.size() - 1
-                        if (num_proc_vars > 0) {
-                            row[1..num_proc_vars].each { proc_vars << it }
-                        }
+                proc_vars = []
+                int num_proc_vars = row.size() - varNames.size() - 1
+                if (num_proc_vars > 0) {
+                    row[1..num_proc_vars].each { proc_vars << it }
+                }
 
-                        [key, *proc_vars]
-                    }
+                [key, *proc_vars]
+            }
 
             return df
         }
 
-        public DataflowQueue joinChannels(DataflowQueue df1, DataflowQueue df2, Integer... level) {
-            assert level != null &&
-                    level.size() >= 1 && level.size() <= 2 : 'Level can be either 1 or 2 integer values'
+        /**
+         * Group the elements of a channel by a common key at a specific level in the
+         * sweep hierarchy.
+         *
+         * It is expected that the specified level would be such that multiple rows in
+         * the table will share a common key value. i.e 1 < level < max_level
+         *
+         * The returned map is indexed by the common key. Its elements are lists which
+         * hold the "overhang" (remaining key levels dropped for this grouping) and the
+         * row values for each grouped set of values.
+         *
+         * @param df - channel to group
+         * @param level - sweep level to group at
+         * @return map of grouped rows.
+         */
+        static Map groupAtLevel(DataflowQueue df, int level) {
+            List list = df.toList().get()
+            return list.inject([:]) { acc, row ->
+                def k = row[0].splitKey(level)
+                if (!acc[k.lo]) {
+                    acc[k.lo] = []
+                }
+                acc[k.lo].add( ['overhang': k.hi, 'values':row[1..-1]] )
+                acc
+            }
+        }
 
-            if (level.size() == 1) {
-                level = [level[0], level[0]]
+        /**
+         * Join two channels together at a given level in the sweep hierarchy. This is used
+         * when merging the results of two branches in the sweep.
+         *
+         * Such cases arise when inputs from a single point are used by two different tasks (branch point)
+         * and then their output channels merged together for further processing (merge point).
+         *
+         * The merged channel elements are lists (table rows) composed of a leading key (which reflects the join point)
+         * and followed by all the per-task output NamedValueS which were emitted by both channels. As such, it can be
+         * a good idea to reduce rows to only the output values which will be required in subsequent tasks. This can be
+         * accomplished using helper meta-method "List.pick(varname1, varname2...).
+         *
+         * Eg. If two output columns were 'reads' and 'contigs', using the map operator in Nextflow
+         *
+         *      chan.map{ it.pick('reads', 'contigs') }
+         *
+         * @param df1 - channel 1
+         * @param df2 - channel 2
+         * @param level - the level of the sweep hierarchy to merge at.
+         * @return channel of merged values
+         */
+        public DataflowQueue joinChannels(DataflowQueue df1, DataflowQueue df2, int level) {
+            Map g1 = groupAtLevel(df1, level)
+            Map g2 = groupAtLevel(df2, level)
+
+            // combine values from each input channel for common keys
+            List ret = g1.keySet().inject([]) { acc, k ->
+                if (g2[k]) {
+                    def x = [g1[k], g2[k]].combinations()
+                    x = [[k], x].combinations{ a, b ->
+                        [a, b['overhang'].flatten(), b['values'].flatten()]
+                    }
+                    acc.addAll(x)
+                }
+                acc
             }
 
-            df1 = df1.map { it ->
-                // remake key
-                [ it[0].id(level[0]), *it[1..-1] ] }
+            // join keys in sweep order and return rows as [key, output_values]
+            ret = ret.collect { row ->
+                Key joined = orderKey(row[0] + row[1][0] + row[1][1])
+                [joined, *row[2]]
+            }
 
-            df2 = df2.map { it ->
-                // remake key
-                [ it[0].id(level[1]), *it[1..-1] ] }
+            return Channel.from(ret)
+        }
 
-            return df1.cross(df2).map { it.flatten() }
+        /**
+         * Impose the sweep variable order on a key
+         * @param key - the key to reorder
+         * @return the reordered key
+         */
+        public Key orderKey(Key key) {
+            return varRegistry.inject(new Key()) { ordKey, name, values ->
+                if (key[name]) {
+                    ordKey.put(key[name])
+                }
+                ordKey
+            }
         }
 
     }
 
-    static def selectByName(keys,list) {
+    static def selectByName(keys, list) {
         def m = list.inject([:]) {acc, it -> acc[it.name] = it; acc}
         keys.inject([]) { acc, k -> assert m[k] != null, "element [$k] not found in map"; acc << m[k]; acc }
     }
