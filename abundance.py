33b33b1215ce7e5e9a7ca5d2f1088bdf649870ab
cerebis <matt.demaere@gmail.com>
2017-08-03 15:16:23 +1000

    check for key duplications in community yaml definition.

--- abundance.py
+++ abundance.py
@@ -16,12 +16,20 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from collections import OrderedDict
-from Bio import SeqIO
-
 import numpy as np
 import re
 import yaml
+
+from collections import OrderedDict
+from Bio import SeqIO
+from yaml.constructor import ConstructorError
+from yaml.nodes import MappingNode
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
 
 
 """
@@ -419,6 +427,32 @@ def read_table(hndl, normalize=False):
     return profile
 
 
+class UniqueKeyLoader(Loader):
+    """
+    Verify that mappings do not contain duplicate keys
+    from https://gist.github.com/pypt/94d747fe5180851196eb user: https://gist.github.com/ngaya-ll
+    """
+    def construct_mapping(self, node, deep=False):
+        if not isinstance(node, MappingNode):
+            raise ConstructorError(None, None,
+                                   "expected a mapping node, but found %s" % node.id,node.start_mark)
+        mapping = {}
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            try:
+                hash(key)
+            except TypeError, exc:
+                raise ConstructorError("while constructing a mapping", node.start_mark,
+                                       "found unacceptable key (%s)" % exc, key_node.start_mark)
+            # check for duplicate keys
+            if key in mapping:
+                raise ConstructorError("while constructing a mapping", node.start_mark,
+                                       "found duplicate key", key_node.start_mark)
+            value = self.construct_object(value_node, deep=deep)
+            mapping[key] = value
+        return mapping
+
+
 def read_yaml(hndl, normalize=False):
     """
     Read a community profile definition file in YAML format. The method returns a ordered
@@ -433,7 +467,7 @@ def read_yaml(hndl, normalize=False):
     :return: an ordered dict of dicts
     """
 
-    cfg = yaml.load(hndl)
+    cfg = yaml.load(hndl, UniqueKeyLoader)
 
     comm_def = OrderedDict()
     for cell_name, cell_info in sorted(cfg.items(), key=lambda x: x[0]):
