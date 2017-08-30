3fa15ba6657fbde78459e5e5d438370947e4d356
Matthew DeMaere <matt.demaere@gmail.com>
2017-08-30 14:40:38 +1000

    reading maps from csv support column header

--- mapio.py
+++ mapio.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas
 import h5py
 
 
@@ -46,19 +47,27 @@ def write_map(data, fname, fmt, field_as=np.float):
         raise RuntimeError('Unimplemented format [{}]'.format(fmt))
 
 
-def read_map(fname, fmt, delim=','):
+def read_map(fname, fmt, delim=',', names=False):
     """
     Read a contact map from file. The map is returned as a 2D
     numpy array. File formats are 'CSV' or a simple HDF5 with
-    a single dataset named 'map'.
+    a single dataset named 'map'. In the case of a named CSV,
+    a tuple will be returned (matrix, rownames, colnames).
     :param fname: input file name
     :param fmt: file format 'csv' or 'h5'
     :param delim: optional delimiter for csv
+    :param names: for CSV, the first row and column specify names.
     :return: 
     """
     if fmt == 'csv':
-        data = np.loadtxt(fname, delimiter=delim)
+        if names:
+            df = pandas.read_csv(fname, delimiter=delim, index_col=0, header=0)
+            return df.as_matrix(), df.index.tolist(), df.columns.tolist()
+        else:
+            data = np.loadtxt(fname, delimiter=delim)
     elif fmt == 'h5':
+        if names:
+            print 'Warning: names parameter ignored for h5 format'
         with h5py.File(fname, 'r') as hndl:
             data = hndl['map'][:]
     else:
