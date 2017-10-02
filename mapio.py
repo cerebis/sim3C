324275d759cd5404681fcf79632f606305d6c93c
Matthew DeMaere <matt.demaere@gmail.com>
2017-10-03 10:47:23 +1100

    csv maps now handled through pandas. sequence names handled in h5 if supplied/requested.

--- mapio.py
+++ mapio.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas
 import h5py
+import io_utils
 
 
-def write_map(data, fname, fmt, field_as=np.float):
+def write_map(data, fname, fmt, names=None):
     """
     Write a contact map (2D numpy array) to file. File formats
     are either CSV or a simple HFD5 with a single dataset named
@@ -18,12 +19,8 @@ def write_map(data, fname, fmt, field_as=np.float):
     :param data: 2d numpy array to write
     :param fname: output file name
     :param fmt: file format 'csv' or 'h5'
-    :param field_as: number representation of each element
+    :param names: names used for both rows an columns
     """
-    field_fmt = '%f'
-    if field_as == np.int:
-        data = data.astype(np.int)
-        field_fmt = '%d'
 
     # try to convert sparse matrices to a dense form before writing
     if not isinstance(data, np.ndarray):
@@ -33,15 +30,24 @@ def write_map(data, fname, fmt, field_as=np.float):
             raise RuntimeError('Supplied data object does not appear to be a sparse matrix, nor a numpy array')
 
     if fmt == 'csv':
+        # append a suffix is not found.
         if not fname.endswith('csv'):
             fname = '{}.csv'.format(fname)
-        np.savetxt(fname, data, fmt=field_fmt, delimiter=',')
+        if names is None:
+            # plain CSV, no row or column names
+            pandas.DataFrame(data).to_csv(fname, header=False, index=False, sep=',')
+        else:
+            # add row and column names to CSV if supplied
+            pandas.DataFrame(data, index=names, columns=names).to_csv(fname, sep=',')
 
     elif fmt == 'h5':
         if not fname.endswith('h5'):
             fname = '{}.h5'.format(fname)
         with h5py.File(fname, 'w') as hndl:
             hndl.create_dataset('map', data=data, compression='gzip')
+            # add names as as separate entry
+            if names is not None:
+                hndl.create_dataset('names', data=names, compression='gzip')
 
     else:
         raise RuntimeError('Unimplemented format [{}]'.format(fmt))
@@ -59,18 +65,32 @@ def read_map(fname, fmt, delim=',', names=False):
     :param names: for CSV, the first row and column specify names.
     :return: 
     """
+    data = None
+    cols = None
+
     if fmt == 'csv':
-        if names:
-            df = pandas.read_csv(fname, delimiter=delim, index_col=0, header=0)
-            return df.as_matrix(), df.index.tolist(), df.columns.tolist()
-        else:
-            data = np.loadtxt(fname, delimiter=delim)
+        with io_utils.open_input(fname) as csv_input:
+            try:
+                if names:
+                    df = pandas.read_csv(csv_input, delimiter=delim, index_col=0, header=0)
+                    data = df.as_matrix().astype(np.float)
+                    if (df.index != df.columns).all():
+                        raise IOError('row and column names do not agree')
+                    cols = np.asarray(df.columns.tolist())
+                else:
+                    df = pandas.read_csv(csv_input, delimiter=delim, dtype=np.float, header=None)
+                    data = df.as_matrix()
+            except ValueError as er:
+                print 'Invalid data. Expected numeric values. Perhaps wrong delimiter used'
+                raise er
+
     elif fmt == 'h5':
-        if names:
-            print 'Warning: names parameter ignored for h5 format'
         with h5py.File(fname, 'r') as hndl:
             data = hndl['map'][:]
+            if names:
+                cols = np.asarray(hndl['names'][:])
     else:
         raise RuntimeError('Unimplemented format [{}]'.format(fmt))
-    return data
+
+    return data, cols
 
